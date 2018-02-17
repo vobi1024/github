230cd9b772021473b8b3649e7de9525297be7018
vobi1024 <34059239+vobi1024@users.noreply.github.com>
2018-02-17 12:30:14 +0100

    esplink to node01, send only

--- UNO_node01/src/main.cpp
+++ UNO_node01/src/main.cpp
@@ -15,36 +15,164 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress tempDeviceAddress;
 
-#include <avr/wdt.h>
+//#include <avr/wdt.h>
 #include <everytime.h>
+#include <ELClient.h>
+#include <ELClientCmd.h>
+#include <ELClientMqtt.h>
+//#include <avr/sleep.h>
 
-unsigned long previousMillis = 0;
+//unsigned long previousMillis = 0;
 
 static String nodeID = "node01";
 
-extern String readAltSerial();
-extern void mqttpub(String channel, String msg);
-extern String FloatToString(float value);
+char dstemp[18] = "n/a";
 
-String dstemp = "n/a";
+// void AVRsleep()
+// {
+//         digitalWrite(LED_BUILTIN, 0);
+//         set_sleep_mode(SLEEP_MODE_IDLE); //sleeps for the rest of this millisecond or less if other trigger
+//         sleep_enable();
+//         sleep_mode();     // put the device to sleep
+//         sleep_disable();
+//         digitalWrite(LED_BUILTIN, 1);
+// }
+
+// String readAltSerial()
+// {
+//         delay(10);
+//         char c;
+//         String str = "";
+//         while (Serial.available() > 0) {
+//                 c = Serial.read();
+//                 //Serial.print(c);
+//                 str += c;
+//         }
+//         return str;
+// }
+
+// void mqttpub(String channel, String msg)
+// {
+//         Serial.println("PUB,/from/" + nodeID + "/" + channel + "," + msg);
+//         //altSerial.print("PUB,/from/" + nodeID + "/" + channel + "," + msg);
+// }
+
+String FloatToString(float value)
+{
+        char buffer[10];
+        String str = dtostrf(value, 5, 2, buffer);
+        return str;
+}
+
+
 
 String readDS()
 {
         digitalWrite(LED_BUILTIN, 1);
         sensors.requestTemperatures();
         float tempC = 0;
+        //Serial.println(sensors.getTempCByIndex(0));
         tempC = sensors.getTempC(tempDeviceAddress);
+        //Serial.println(tempC, 2);
         char buffer[10];
         String str = dtostrf(tempC, 5, 2, buffer);
         if ((tempC > 60) or (tempC < 0)) str = "n/a";
         digitalWrite(LED_BUILTIN, 0);
         return str;
+        //tem = ("Node01,tmp1,0," + tem + "\n");
+        //tem.toCharArray(temper, 32);
+}
+
+// Initialize a connection to esp-link using the normal hardware serial port both for
+// SLIP and for debug messages.
+ELClient esp(&Serial, &Serial);
+
+// Initialize CMD client (for GetTime)
+ELClientCmd cmd(&esp);
+
+// Initialize the MQTT client
+ELClientMqtt mqtt(&esp);
+
+// Callback made from esp-link to notify of wifi status changes
+// Here we just print something out for grins
+void wifiCb(void* response) {
+        ELClientResponse *res = (ELClientResponse*)response;
+        if (res->argc() == 1) {
+                uint8_t status;
+                res->popArg(&status, 1);
+
+                if(status == STATION_GOT_IP) {
+                        Serial.println("WIFI CONNECTED");
+                } else {
+                        Serial.print("WIFI NOT READY: ");
+                        Serial.println(status);
+                }
+        }
+}
+
+bool connected;
+
+// Callback when MQTT is connected
+void mqttConnected(void* response) {
+        Serial.println("MQTT connected!");
+        mqtt.subscribe("/esp-link/1");
+        mqtt.subscribe("/hello/world/#");
+        //mqtt.subscribe("/esp-link/2", 1);
+        //mqtt.publish("/esp-link/0", "test1");
+        connected = true;
+}
+
+// Callback when MQTT is disconnected
+void mqttDisconnected(void* response) {
+        Serial.println("MQTT disconnected");
+        connected = false;
+}
+
+// Callback when an MQTT message arrives for one of our subscriptions
+void mqttData(void* response) {
+        ELClientResponse *res = (ELClientResponse *)response;
+
+        Serial.print("Received: topic=");
+        String topic = res->popString();
+        Serial.println(topic);
+
+        Serial.print("data=");
+        String data = res->popString();
+        Serial.println(data);
+}
+
+void mqttPublished(void* response) {
+        Serial.println("MQTT published");
 }
 
 void setup() {
-        wdt_disable();
+        Serial.begin(115200);
+        Serial.println("EL-Client starting!");
+
+        // Sync-up with esp-link, this is required at the start of any sketch and initializes the
+        // callbacks to the wifi status change callback. The callback gets called with the initial
+        // status right after Sync() below completes.
+        esp.wifiCb.attach(wifiCb); // wifi status change callback, optional (delete if not desired)
+        bool ok;
+        do {
+                ok = esp.Sync(); // sync up with esp-link, blocks for up to 2 seconds
+                if (!ok) Serial.println("EL-Client sync failed!");
+        } while(!ok);
+        Serial.println("EL-Client synced!");
+
+        // Set-up callbacks for events and initialize with es-link.
+        mqtt.connectedCb.attach(mqttConnected);
+        mqtt.disconnectedCb.attach(mqttDisconnected);
+        mqtt.publishedCb.attach(mqttPublished);
+        mqtt.dataCb.attach(mqttData);
+        mqtt.setup();
+
+        //Serial.println("ARDUINO: setup mqtt lwt");
+        //mqtt.lwt("/lwt", "offline", 0, 0); //or mqtt.lwt("/lwt", "offline");
+
+        Serial.println("EL-MQTT ready");
+        //wdt_disable();
         pinMode(LED_BUILTIN, OUTPUT);
-        Serial.begin(19200);
         sensors.begin();
         sensors.getAddress(tempDeviceAddress, 0);
         sensors.setResolution(tempDeviceAddress, 10);
@@ -52,36 +180,47 @@ void setup() {
         //while (!Serial) ; // wait for Arduino Serial Monitor to open
         //Serial.println("AltSoftSerial Test Begin");
         //  altSerial.begin(19200);
-        wdt_enable(WDTO_4S);
-        mqttpub("status", "Arduino 01 online");
+        //wdt_enable(WDTO_4S);
+        //mqttpub("status", "Arduino 01 online");
 }
 
 void loop() {
+        esp.Process();
+        //wdt_reset();
+        // unsigned long currentMillis = millis();
+        // if ((unsigned long)(currentMillis - previousMillis) >= 42000) {
+        //         //Serial.println("Sending reset...");
+        //         Serial.println("PUB,/from/" + nodeID + "/status,RESET");
+        //         delay(250);
+        //         Serial.println("RST,,");
+        //         previousMillis = currentMillis;
+        // }
 
-        wdt_reset();
-        unsigned long currentMillis = millis();
-        if ((unsigned long)(currentMillis - previousMillis) >= 42000) {
-                //Serial.println("Sending reset...");
-                Serial.println("PUB,/from/" + nodeID + "/status,RESET");
-                delay(250);
-                Serial.println("RST,,");
-                previousMillis = currentMillis;
+        every(10000)
+        {
+                //dstemp[10] = readDS();
+                readDS().toCharArray(dstemp, 18);
+                Serial.print("dstemp var: ");
+                Serial.println(dstemp);
+                Serial.print("readds func: ");
+                Serial.println(readDS());
+                readDS().toCharArray(dstemp, 18);
+                mqtt.publish("/from/node01e/tmp1", dstemp);
         }
 
-        every(10000) dstemp = readDS();
-
-        if (Serial.available()) {
-                String rxstr = readAltSerial();
-
-                if (rxstr.substring(15, 19) == "tmp1")
-                {
-                        mqttpub("tmp1", dstemp);
-                }
-
-                if (rxstr.substring(0, 3) == "CMD")
-                {
-                        previousMillis = currentMillis;
-                }
-        } //incoming msg end
+        // if (Serial.available()) {
+        //         //String rxstr = readAltSerial();
+        //
+        //         if (rxstr.substring(15, 19) == "tmp1")
+        //         {
+        //           mqtt.publish("/from/node01/tmp1", dstemp);
+        //                 //mqttpub("tmp1", dstemp);
+        //         }
+        //
+        //         if (rxstr.substring(0, 3) == "CMD")
+        //         {
+        //                 //previousMillis = currentMillis;
+        //         }
+        // } //incoming msg end
         // AVRsleep();
 } //loop
