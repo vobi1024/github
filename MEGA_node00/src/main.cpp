24898f126a6afd6d88ea1760478a81fd43719423
vobi1024 <34059239+vobi1024@users.noreply.github.com>
2018-02-25 21:52:58 +0100

    node00 to esp-link

--- MEGA_node00/src/main.cpp
+++ MEGA_node00/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <avr/wdt.h>
+#undef PN532DEBUGPRINT
+//#include <avr/wdt.h>
 
 #include "DHT.h"
 DHT dht;
@@ -11,9 +12,17 @@ DHT dht;
 #include <everytime.h>
 #include <EEPROM.h>
 
+#include <ELClient.h>
+#include <ELClientCmd.h>
+#include <ELClientMqtt.h>
+
 static String nodeID = "node00";
 
-unsigned long previousMillis = 0;
+//unsigned long previousMillis = 0;
+
+bool newData = 0;
+String topic = "";
+String data = "";
 
 #define PN532_IRQ   (2)
 #define PN532_RESET (3)  // Not connected by default on the NFC Shield
@@ -45,33 +54,108 @@ String temp = "n/a";
 String hum = "n/a";
 String armstate = "n/a";
 
+// Initialize a connection to esp-link using the normal hardware serial port both for
+// SLIP and for debug messages.
+ELClient esp(&Serial, &Serial);
+
+// Initialize CMD client (for GetTime)
+ELClientCmd cmd(&esp);
+
+// Initialize the MQTT client
+ELClientMqtt mqtt(&esp);
+
+// Callback made from esp-link to notify of wifi status changes
+// Here we just print something out for grins
+void wifiCb(void* response) {
+        ELClientResponse *res = (ELClientResponse*)response;
+        if (res->argc() == 1) {
+                uint8_t status;
+                res->popArg(&status, 1);
+
+                if(status == STATION_GOT_IP) {
+                        Serial.println("WIFI CONNECTED");
+                } else {
+                        Serial.print("WIFI NOT READY: ");
+                        Serial.println(status);
+                }
+        }
+}
+
+bool connected;
+
+// Callback when MQTT is connected
+void mqttConnected(void* response) {
+        Serial.println("MQTT connected!");
+        mqtt.subscribe("/to/node00e/#");
+        //mqtt.subscribe("/hello/world/#");
+        //mqtt.subscribe("/esp-link/2", 1);
+        //mqtt.publish("/esp-link/0", "test1");
+        connected = true;
+        mqtt.publish("/from/node00e/status", "START", 1);
+}
+
+// Callback when MQTT is disconnected
+void mqttDisconnected(void* response) {
+        Serial.println("MQTT disconnected");
+        connected = false;
+}
+
+// Callback when an MQTT message arrives for one of our subscriptions
+void mqttData(void* response) {
+        ELClientResponse *res = (ELClientResponse *)response;
+
+        Serial.print("Received: topic=");
+        topic = res->popString();
+        Serial.println(topic);
+
+        Serial.print("data=");
+        data = res->popString();
+        Serial.println(data);
+        newData = 1;
+}
+
+void mqttPublished(void* response) {
+        Serial.println("MQTT published");
+}
+
 //**------------functions begin--------------**//
 
-void mqttpub(String channel, String msg)
-{
-        delay(25);
-        Serial1.println("PUB,/from/" + nodeID + "/" + channel + "," + msg);
-        //altSerial.print("PUB,/from/" + nodeID + "/" + channel + "," + msg);
+char* string2char(String command){
+        if(command.length()!=0) {
+                char *p = const_cast<char*>(command.c_str());
+                return p;
+        }
 }
+
+// void mqttpub(String channel, String msg)
+// {
+//         delay(25);
+//         Serial1.println("PUB,/from/" + nodeID + "/" + channel + "," + msg);
+//         //altSerial.print("PUB,/from/" + nodeID + "/" + channel + "," + msg);
+// }
 
 void setLockState(bool state) {
         if (!state)
         {
-                mqttpub("arm1", User + " disarmed!");
+                //mqttpub("arm1", User + " disarmed!");
+                mqtt.publish("/from/node00e/arm1", string2char(User + " disarmed!"), 1);
                 // Serial1.print("Node00,arm1,1,0,");
                 // Serial1.print(User);
                 // Serial1.println(" disarmed!");
                 armstate = "Disarmed";
-                mqttpub("arms", armstate);
+                //mqttpub("arms", armstate);
+                mqtt.publish("/from/node00e/arms", string2char(armstate), 1);
         }
         else
         {
-                mqttpub("arm1", User + " armed!");
+                //mqttpub("arm1", User + " armed!");
+                mqtt.publish("/from/node00e/arm1", string2char(User + " armed!"), 1);
                 // Serial1.print("Node00,arm1,1,1,");
                 // Serial1.print(User);
                 // Serial1.println(" armed!");
                 armstate = "Armed";
-                mqttpub("arms", armstate);
+                //mqttpub("arms", armstate);
+                mqtt.publish("/from/node00e/arms", string2char(armstate), 1);
         }
         EEPROM.write(0, state);
         digitalWrite(22, state);
@@ -87,27 +171,55 @@ String FloatToString(float value)
         return str;
 }
 
-String readAltSerial()
-{
-        delay(10);
-        char c;
-        String str = "";
-        while (Serial1.available() > 0) {
-                c = Serial1.read();
-                //Serial.print(c);
-                str += c;
-        }
-        return str;
-}
+// String readAltSerial()
+// {
+//         delay(10);
+//         char c;
+//         String str = "";
+//         while (Serial1.available() > 0) {
+//                 c = Serial1.read();
+//                 //Serial.print(c);
+//                 str += c;
+//         }
+//         return str;
+// }
 
 //**------------functions end--------------**//
 
 void setup(void) {
-        wdt_disable();
-        pinMode(LED_BUILTIN, OUTPUT);
+        //wdt_disable();
+        //pinMode(LED_BUILTIN, OUTPUT);
         pinMode(22, OUTPUT);
         pinMode(24, OUTPUT);
         pinMode(23, INPUT);
+
+        Serial.begin(115200);
+        Serial.println("EL-Client starting!");
+
+        // Sync-up with esp-link, this is required at the start of any sketch and initializes the
+        // callbacks to the wifi status change callback. The callback gets called with the initial
+        // status right after Sync() below completes.
+        esp.wifiCb.attach(wifiCb); // wifi status change callback, optional (delete if not desired)
+        bool ok;
+        do {
+                ok = esp.Sync(); // sync up with esp-link, blocks for up to 2 seconds
+                if (!ok) Serial.println("EL-Client sync failed!");
+        } while(!ok);
+        Serial.println("EL-Client synced!");
+
+        // Set-up callbacks for events and initialize with es-link.
+        mqtt.connectedCb.attach(mqttConnected);
+        mqtt.disconnectedCb.attach(mqttDisconnected);
+        mqtt.publishedCb.attach(mqttPublished);
+        mqtt.dataCb.attach(mqttData);
+        mqtt.setup();
+
+        //Serial.println("ARDUINO: setup mqtt lwt");
+        //mqtt.lwt("/lwt", "offline", 0, 0); //or mqtt.lwt("/lwt", "offline");
+
+        Serial.println("EL-MQTT ready");
+        //wdt_disable();
+        pinMode(LED_BUILTIN, OUTPUT);
 
         dht.setup(4);
         //Serial.begin(9600);
@@ -117,7 +229,8 @@ void setup(void) {
         nfc.begin();
         uint32_t versiondata = nfc.getFirmwareVersion();
         if (!versiondata) {
-                mqttpub("status", User + " Didn't find PN53x board");
+                //mqttpub("status", User + " Didn't find PN53x board");
+                mqtt.publish("/from/node00e/status", string2char(User + " Didn't find PN53x board"), 1);
                 while (1); // halt
         }
 
@@ -129,22 +242,23 @@ void setup(void) {
         digitalWrite(22, lockStatus);
         digitalWrite(24, !lockStatus);
         setLockState(lockStatus);
-        wdt_enable(WDTO_4S);
+        //wdt_enable(WDTO_4S);
         //mqttpub("status", "Arduino 00 online");
         //mqttpub("arms", armstate);
 }
 
 void loop(void) {
+        esp.Process();
         digitalWrite(LED_BUILTIN, 0);
-        wdt_reset();
-        unsigned long currentMillis = millis();
-        if ((unsigned long)(currentMillis - previousMillis) >= 42000) {
-                //Serial.println("Sending reset...");
-                Serial1.println("PUB,/from/" + nodeID + "/status,RESET");
-                delay(250);
-                Serial1.println("RST,,");
-                previousMillis = currentMillis;
-        }
+        //wdt_reset();
+        //unsigned long currentMillis = millis();
+        // if ((unsigned long)(currentMillis - previousMillis) >= 42000) {
+        //         //Serial.println("Sending reset...");
+        //         Serial1.println("PUB,/from/" + nodeID + "/status,RESET");
+        //         delay(250);
+        //         Serial1.println("RST,,");
+        //         previousMillis = currentMillis;
+        // }
         uint8_t NFCsuccess;
         uint8_t NFCuid[] = {
                 0, 0, 0, 0, 0, 0, 0
@@ -193,60 +307,73 @@ void loop(void) {
         }
 
         every(1000) {
+                digitalWrite(LED_BUILTIN, 1);
                 if (!lockStatus) digitalWrite(22, !digitalRead(23));
                 if (lockStatus) digitalWrite(24, !digitalRead(23));
                 if (lockStatus & digitalRead(23)) {
                         // Serial1.println("Node00,mot1,1,1,Movement detected!"); //Movement detected!
-                        mqttpub("alm1", "Movement detected!");
+                        //mqttpub("alm1", "Movement detected!");
+                        Serial.println("Movement detected!");
+                        mqtt.publish("/from/node00e/alm1", string2char("Movement detected!"), 1);
+                        digitalWrite(LED_BUILTIN, 0);
                 }
         }
 
-        if (Serial1.available()) {
-                String rxstr = readAltSerial();
-                //Serial.println(rxstr);
-
-                if (rxstr.substring(15, 19) == "tmp1")
+        if (newData) {
+                digitalWrite(LED_BUILTIN, 1);
+                //String rxstr = readAltSerial();
+                if (topic.substring(12, 16) == "tmp1")
                 {
-                        digitalWrite(LED_BUILTIN, 1);
+
                         float temperature = dht.getTemperature();
                         temp = FloatToString (temperature-1);
-                        mqttpub("tmp1", temp);
-                        digitalWrite(LED_BUILTIN, 0);
-                }
+                        //mqtt.publish("/from/node02e/tmp1", tempa);
+                        mqtt.publish("/from/node00e/tmp1", string2char(temp), 1);
+                        Serial.print("MQTT published: ");
+                        Serial.println(temp);
 
-                if (rxstr.substring(15, 19) == "hum1")
+                }
+                if (topic.substring(12, 16) == "hum1")
                 {
-                        digitalWrite(LED_BUILTIN, 1);
+                        //mqtt.publish("/from/node02e/hum1", huma);
+
                         hum = FloatToString (dht.getHumidity());
-                        mqttpub("hum1", hum);
-                        digitalWrite(LED_BUILTIN, 0);
-                }
+                        mqtt.publish("/from/node00e/hum1", string2char(hum), 1);
+                        Serial.print("MQTT published: ");
+                        Serial.println(hum);
 
-                if (rxstr.substring(15, 19) == "dht1")
+                }
+                if (topic.substring(12, 16) == "dht1")
                 {
-                        digitalWrite(LED_BUILTIN, 1);
+
                         float temperature = dht.getTemperature();
                         temp = FloatToString (temperature-1);
                         hum = FloatToString (dht.getHumidity());
-                        mqttpub("dht1", temp + ";" + hum);
-                        digitalWrite(LED_BUILTIN, 0);
+                        //mqttpub("dht1", temp + ";" + hum);
+                        mqtt.publish("/from/node00e/dht1", string2char(temp + ";" + hum), 1);
+                        Serial.print("MQTT published: ");
+                        Serial.println(temp + ";" + hum);
+
                 }
 
-                if (rxstr.substring(15, 19) == "arms")
+                if (topic.substring(12, 16) == "arms")
                 {
                         setLockState(!lockStatus);
-                        mqttpub("arms", armstate);
+                        //mqttpub("arms", armstate);
+                        mqtt.publish("/from/node00e/arms", string2char(armstate), 1);
                 }
 
-                if (rxstr.substring(15, 19) == "arma")
+                if (topic.substring(12, 16) == "arma")
                 {
-                        mqttpub("arms", armstate);
+                        //mqttpub("arms", armstate);
+                        mqtt.publish("/from/node00e/arma", string2char(armstate), 1);
                 }
-
-                if (rxstr.substring(0, 3) == "CMD")
-                {
-                        previousMillis = currentMillis;
-                }
+                //
+                // if (rxstr.substring(0, 3) == "CMD")
+                // {
+                //         //previousMillis = currentMillis;
+                // }
+                newData = 0;
+                digitalWrite(LED_BUILTIN, 0);
         }
-
 }
